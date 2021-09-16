4de8e968567a562d6e68d2406070ccdbe1cd6109
bobby3003 <roberto3003@riseup.net>
2021-09-17 05:02:19 +0700

    finished

---
+++ target/rls/debug/deps/myproject-0b2136b19a96e675.d
@@ -0,0 +1,5 @@
+c:\Users\bobby\Dev\rust\first-rust-app\target\rls\debug\deps\myproject-0b2136b19a96e675.rmeta: src\main.rs
+
+c:\Users\bobby\Dev\rust\first-rust-app\target\rls\debug\deps\myproject-0b2136b19a96e675.d: src\main.rs
+
+src\main.rs:
---
+++ target/rls/debug/deps/myproject-40dc45e0c8616d3e.d
@@ -0,0 +1,5 @@
+c:\Users\bobby\Dev\rust\first-rust-app\target\rls\debug\deps\myproject-40dc45e0c8616d3e.rmeta: src\main.rs
+
+c:\Users\bobby\Dev\rust\first-rust-app\target\rls\debug\deps\myproject-40dc45e0c8616d3e.d: src\main.rs
+
+src\main.rs:
